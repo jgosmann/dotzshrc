30f13fe8b717bfacea74b9d350d5e88f679da3e6
Jan Gosmann <jan@hyper-world.de>
2012-10-23 16:43:57 +0200

    postgres-kill alias

--- alias.zsh
+++ alias.zsh
@@ -7,7 +7,9 @@ alias gsd='git svn dcommit --rmdir'
 
 alias mysqlstart="sudo /opt/local/bin/mysqld_safe5"
 alias mysqlstop="/opt/local/bin/mysqladmin5 -u root -p shutdown"
+
 alias postgres-restart="sudo -u postgres /Library/PostgreSQL/9.0/bin/pg_ctl -D /Library/PostgreSQL/9.0/data restart"
+alias postgres-kill="ps -A | grep postgres | cut -f 1 -d ' ' | xargs sudo kill"
 
 if ! (type -p netcat > /dev/null); then
     alias netcat=nc
