055d50462a5833f6233d519475a0787182e3b519
Jan Gosmann <jan@hyper-world.de>
2012-11-13 16:07:24 +0100

    Parallelized make alias.

--- alias.zsh
+++ alias.zsh
@@ -5,6 +5,8 @@ alias mv='nocorrect mv'
 alias gsr='git svn rebase'
 alias gsd='git svn dcommit --rmdir'
 
+alias make='make -j 2'
+
 alias mysqlstart="sudo /opt/local/bin/mysqld_safe5"
 alias mysqlstop="/opt/local/bin/mysqladmin5 -u root -p shutdown"
 
